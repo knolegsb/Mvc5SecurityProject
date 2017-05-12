92e5864fa120ad4609e3b3f9bc1e8f0a8fae3831
Sean <seon2you@hotmail.com>
2017-05-11 21:59:28 -0700

    Add project files.

---
+++ Mvc5SecurityProject/Controllers/RoleController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Mvc5SecurityProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc5SecurityProject.Controllers
+{
+    public class RoleController : Controller
+    {
+        ApplicationDbContext context;
+
+        public RoleController()
+        {
+            context = new ApplicationDbContext();
+        }
+
+        // GET: Role
+        public ActionResult Index()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                if (!isAdminUser())
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            var roles = context.Roles.ToList();
+            return View(roles);
+        }
+
+        private bool isAdminUser()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                var user = User.Identity;
+                var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+                var s = UserManager.GetRoles(user.GetUserId());
+                if (s[0].ToString() == "Admin")
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return false;
+        }
+
+        public ActionResult Create()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                if (!isAdminUser())
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            var role = new IdentityRole();
+            return View(role);
+        }
+
+        [HttpPost]
+        public ActionResult Create(IdentityRole role)
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                if (!isAdminUser())
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            context.Roles.Add(role);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Mvc5SecurityProject/Startup.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.Owin;
+using Owin;
+using Mvc5SecurityProject.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+[assembly: OwinStartupAttribute(typeof(Mvc5SecurityProject.Startup))]
+namespace Mvc5SecurityProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            CreateRolesAndUsers();
+        }
+
+        // In this method we will create default user roles and admin user for login
+        private void CreateRolesAndUsers()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+            // In startup, creating first Admin Role and creating a default Admin User
+            if (!roleManager.RoleExists("Admin"))
+            {
+                // first we create Admin role
+                var role = new IdentityRole();
+                role.Name = "Admin";
+                roleManager.Create(role);
+
+                // Here we create an Admin Super User who will maintain the website
+
+                var user = new ApplicationUser();
+                user.UserName = "Sean";
+                user.Email = "sean@example.com";
+
+                string userPassword = "Sean@123";
+
+                var checkUser = userManager.Create(user, userPassword);
+
+                // Add default user to role admin
+                if (checkUser.Succeeded)
+                {
+                    var checkResult = userManager.AddToRole(user.Id, "Admin");
+                }                
+            }
+
+            // Creating manager role
+            if (!roleManager.RoleExists("Manager"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Manager";
+                roleManager.Create(role);
+            }
+
+            // Creating employee role
+            if (!roleManager.RoleExists("Employee"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Employee";
+                roleManager.Create(role);
+            }
+        }
+    }
+}
